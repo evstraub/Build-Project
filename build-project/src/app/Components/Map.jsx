1d081f3b730fede0ae116c294e14480cc07d845d
evstraub <evstraub@gmail.com>
2025-03-31 21:42:23 -0400

    add mapbox and Query component for week 2 and 3

--- build-project/src/app/Components/Map.jsx
+++ build-project/src/app/Components/Map.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
 
-const Map = () => {
+import React from 'react'
+import Map from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+
+const MapComp = () => {
   return (
-    <div className='flex w-screen h-screen bg-blue-700'>Map</div>
-  )
+    
+    <>
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoiZXZzdHJhdWIiLCJhIjoiY204ZWphbXZiMDIyYjJpcHhmaWgwa2t2biJ9.XOyytpUDLUEPZUpE0lEAgQ"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 1200, height: 800}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/3.10.0>/mapbox-gl.css' rel='stylesheet' />
+   </>
+  );
+  
 }
 
-export default Map
+export default MapComp
