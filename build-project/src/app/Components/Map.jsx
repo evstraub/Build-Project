c40afe459c39dd4742298021185230f093f24830
svayser <svayser@meridiancapital.com>
2025-04-02 14:51:36 -0400

    added bike parking shelters

--- build-project/src/app/Components/Map.jsx
+++ build-project/src/app/Components/Map.jsx
@@ -1,28 +1,46 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Map from 'react-map-gl/mapbox';
-import 'mapbox-gl/dist/mapbox-gl.css';
-
+import React from "react";
+import Map, { Layer, Source } from "react-map-gl/mapbox";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapComp = () => {
   return (
-    
     <>
-    <Map
-      mapboxAccessToken="pk.eyJ1IjoiZXZzdHJhdWIiLCJhIjoiY204ZWphbXZiMDIyYjJpcHhmaWgwa2t2biJ9.XOyytpUDLUEPZUpE0lEAgQ"
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: 1200, height: 800}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/3.10.0>/mapbox-gl.css' rel='stylesheet' />
-   </>
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoiZXZzdHJhdWIiLCJhIjoiY204ZWphbXZiMDIyYjJpcHhmaWgwa2t2biJ9.XOyytpUDLUEPZUpE0lEAgQ"
+        initialViewState={{
+          longitude: -73.971321,
+          latitude: 40.776676,
+          zoom: 12,
+        }}
+        style={{ width: 1200, height: 800 }}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+      >
+        <Source
+          id="my-source-id"
+          type="vector"
+          url="mapbox://evstraub.apbmnk1d"
+        >
+          <Layer
+            id="my-layer-id"
+            source="my-source-id"
+            type="circle"
+            source-layer="nyc-bike-parking-shelters-5mbpg2"
+            paint={{
+              "circle-radius": 60,
+              "circle-color": "#ff0000",
+              "circle-opacity": 0.8,
+            }}
+          />
+        </Source>
+      </Map>
+    </>
   );
-  
-}
+};
 
-export default MapComp
+export default MapComp;
+
+// evstraub.apbmnk1d - dataset id
+// layer name - nyc-bike-parking-shelters-5mbpg2
+// pk.eyJ1IjoiZXZzdHJhdWIiLCJhIjoiY204ZWphbXZiMDIyYjJpcHhmaWgwa2t2biJ9.XOyytpUDLUEPZUpE0lEAgQ - access token
--- build-project/src/app/layout.js
+++ build-project/src/app/layout.js
@@ -1,18 +1,14 @@
-'use client'
+"use client";
 import "./globals.css";
 import Query from "./Components/Query";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import Nav from './Components/Nav.jsx';
-import SideBar from './Components/SideBar.jsx';
-import Footer from './Components/Footer.jsx';
-import MapComp from './Components/Map.jsx';
-
-
-
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Nav from "./Components/Nav.jsx";
+import SideBar from "./Components/SideBar.jsx";
+import Footer from "./Components/Footer.jsx";
+import MapComp from "./Components/Map.jsx";
 
 const client = new ApolloClient({
-  uri: 'https://graphql.eng.meridiancapital.com/graphql',
+  uri: "https://graphql.eng.meridiancapital.com/graphql",
   cache: new InMemoryCache(),
 });
 
@@ -20,25 +16,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-
         {children}
-   
-        <Nav/>
-       <main className= " flex items-center bg-sky-500">
-        <SideBar/>
-        <MapComp/>
-        <ApolloProvider client={client}>
-      {<Query/>}
-    </ApolloProvider>
-       </main>
-    <Footer/>
 
+        <Nav />
+        <main className=" flex items-center bg-sky-500">
+          <SideBar />
+          <MapComp />
+          <ApolloProvider client={client}></ApolloProvider>
+        </main>
+        <Footer />
       </body>
     </html>
   );
 }
-
-
-
-
-
